65ab4606752dae47c9e11a2663a184efc899aba3
Evgenii Shchemelev <shchemelevev@gmail.com>
2017-01-10 00:02:44 +0200

    fix

--- uno/server/score_storage.py
+++ uno/server/score_storage.py
@@ -5,17 +5,32 @@ import asyncio_redis
 
 logger = logging.getLogger(__name__)
 
+WIN_SUFFIX = '_win_count'
+LOSE_SUFFIX = '_lose_count'
+REDIS_PARAMS = {'host': '127.0.0.1', 'port': 6379}
+
 #TODO: permanent connection
 async def get_user_score(username):
-    connection = await asyncio_redis.Connection.create(host='127.0.0.1', port=6379)
-    result = await connection.get(username)
-    if result is None:
-        result = 0
+    connection = await asyncio_redis.Connection.create(**REDIS_PARAMS)
+    win_count = await connection.get(username + WIN_SUFFIX)
+    if win_count is None:
+        win_count = 0
+    lose_count = await connection.get(username + LOSE_SUFFIX)
+    if lose_count is None:
+        lose_count = 0
+    if win_count + lose_count == 0:
+        return 0
+    else:
+        return win_count / (win_count + lose_count)
     connection.close()
     return result
 
+
 #TODO: permanent connection
-async def record_user_score(username, score):
-    connection = await asyncio_redis.Connection.create(host='127.0.0.1', port=6379)
-    await connection.incrby(username, score)
+async def record_user_score(username, result):
+    connection = await asyncio_redis.Connection.create(**REDIS_PARAMS)
+    if result:
+        await connection.incrby(username + WIN_SUFFIX, score)
+    else:
+        await connection.incrby(username + LOSE_SUFFIX, score)
     connection.close()
